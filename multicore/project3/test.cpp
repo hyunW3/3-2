8675c5fa53d54ff92d63e1befe039a8c721b5341
hyunW3 <0176778602@naver.com>
2020-11-24 00:01:43 +0900

    test.cpp MPI

--- multicore/project3/test.cpp
+++ multicore/project3/test.cpp
@@ -2,11 +2,20 @@
 #include <stdio.h>
 #include <iostream>
 #include <mpi.h>
+#include <unistd.h>
 #include <string.h>
 
 int main(int argc, char *argv[]){
-    MPI_Init(NULL,NULL);
-    printf("Hello world!\n");
+    int size;
+    int rank;
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    printf("size :%d, rank: %d\n",size,rank);
+
+    char buf[100];
+    gethostname(buf,100);
+    printf("hostname : %s\n",buf);
 
     MPI_Finalize();
     return 0;

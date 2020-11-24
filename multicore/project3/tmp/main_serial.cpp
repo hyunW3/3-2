96f61861e73dde9160208ffb21d5a9fa3bd01d40
hyunw3 <0176778602@naver.com>
2020-11-24 08:34:30 +0000

    local calculation is success

--- multicore/project3/tmp/main_serial.cpp
+++ multicore/project3/tmp/main_serial.cpp
@@ -51,25 +51,16 @@ int main(int argc, char *argv[]){
     fclose(f);
 /// serial version 
     //for num of Gen
-    
-    //for(int g=0; g<num_Gen; g++){
+    if(rank == 0){
+    for(int g=0; g<num_Gen; g++){
         for(int i=0; i<Y_limit; i++){
-            for(int j=0; j<X_limit; j++){ // i,j
+            for(int j=0; j<X_limit; j++){
                 // get pos and cal alive or dead neigherhood
-                if((i*X_limit+j)%size == rank){
-                    alive_map[i][j] = get_num_alive(i,j);
-                    //printf("%d",alive_map[i][j]);
-                }
                 //int n_alive = get_num_alive(i,j);
+                alive_map[i][j] = get_num_alive(i,j);
+                //printf("%d",n_alive);
             }
-        }
-        if (rank == 1){ // for test
-        for(int i=0; i<Y_limit; i++){
-            for(int j=0; j<X_limit; j++){ // i,j
-                printf("%d",alive_map[i][j]);
-            }
-            printf("\n");
-        }
+            //printf("\n");
         }
         for(int i=0; i<Y_limit; i++){
             for(int j=0; j<X_limit; j++){
@@ -89,8 +80,8 @@ int main(int argc, char *argv[]){
             printf("\n");
         }
         printf("==================\n");
-    //} //end for(int g=0; g<num_Gen)
-    //} // end if (rank ==0)
+    }
+    } // end if (rank ==0)
 /// finish program
     free(filename);
     for(int i=0; i<Y_limit; i++){

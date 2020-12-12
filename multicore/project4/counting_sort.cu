f63cf0890c469be70cb3642454073566d127edb8
hyunw3 <0176778602@naver.com>
2020-12-12 13:16:38 +0900

    multicore project4

--- multicore/project4/counting_sort.cu
+++ multicore/project4/counting_sort.cu
@@ -1,26 +1,27 @@
 #include <cuda.h>
+#include <cmath>
+   
 int nBlk,nTid;
-__global__ void cuda_sort(int* arr_d,int* histogram_d, int size, int max_val);
+__global__ void cuda_sort(int* arr_d,int* out, int* histogram_d, int size, int max_val);
 __host__ void counting_sort(int arr[], int size, int max_val)
 {
-   nTid = 512;
-   nBlk = size/nTid;
-   if(nBlk * nTid < size){
-      nBlk +=1;
-   }
-   int * arr_d;
+   int* arr_d;
+   int* out;
    int* histogram_d;
    cudaMalloc((void**)&arr_d,size*sizeof(int));
+   cudaMalloc((void**)&out  ,size*sizeof(int));
    cudaMemcpy(arr_d,arr,size*sizeof(int),cudaMemcpyHostToDevice);
    cudaMalloc((void**)&histogram_d,max_val*sizeof(int));
    cudaMemset((void**)&histogram_d,0,max_val*sizeof(int));
-   //cuda_sort<<<nBlk,nTid>>>(arr_d,histogram_d, size, max_val);
-   cuda_sort<<<1,size,size*sizeof(int)>>>(arr_d,histogram_d, size, max_val);
+   if(size>1024)  cuda_sort<<<ceil(size/1024),1024,size*sizeof(int)>>>(arr_d,out, histogram_d, size, max_val);
+   else           cuda_sort<<<1,size,size*sizeof(int)>>>(arr_d,out, histogram_d, size, max_val);
    // copy to host & finsh
-   cudaMemcpy(arr,arr_d,size*sizeof(int),cudaMemcpyDeviceToHost);
+   cudaMemcpy(arr,out,size*sizeof(int),cudaMemcpyDeviceToHost);
    cudaFree(arr_d); cudaFree(histogram_d);
+   cudaFree(out);
 }
-__global__ void cuda_sort(int* arr_d,int* histogram_d, int size, int max_val){
+
+__global__ void cuda_sort(int* arr_d,int* out, int* histogram_d, int size, int max_val){
    extern __shared__ int position[]; 
    int i = threadIdx.x + blockDim.x * blockIdx.x;
    //__device__ int histogram_d[max_val];
@@ -29,19 +30,22 @@ __global__ void cuda_sort(int* arr_d,int* histogram_d, int size, int max_val){
    }
    __syncthreads();
    if(i<max_val){
-      position[i] = histogram_d[i-1];
-      for(int j=0; j<i-1; j++){
-         position[i] += histogram_d[j];
+      if(i==0) position[0] = 0;
+      else {
+         position[i] = histogram_d[i-1];
+         for(int j=0; j<i-1; j++){
+            position[i] += histogram_d[j];
+         }
       }
    }
    __syncthreads();
-   if(i<max_val){
-   //   arr_d[i] = histogram_d[i];
-   //   arr_d[i] = position[i];
+   if(i<max_val){ 
+
    for(int j=0; j<histogram_d[i]; j++){
-      arr_d[position[i]+j] = i;
+      out[position[i]+j] = i;
    }
-   
+
+   //   memset(&arr_d[position[i]],i,sizeof(int)*histogram_d[i]);
    }
    // device code
 }
--- multicore/project4/driver1.cpp
+++ multicore/project4/driver1.cpp
@@ -1,26 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <cuda_runtime.h>
-
+#include <unistd.h>
+#include <cstring>
+#include <cstdint>
+#include <ctime>
 //#define N 1000000
 //#define MAX_VAL 1000000
-#define N 1000
-#define MAX_VAL 1000
+// error in 1000000000
+#define N 1000000000
+#define MAX_VAL 1000000000
 
 extern void counting_sort(int arr[], int, int);
 
 int main()
 {
-  int array[N];
+  int* array = new int[N];
 
   for(int i=0;i<N;i++){
       array[i] = rand()%MAX_VAL;
-//      printf("%02d ",array[i]);
+      //printf("%02d ",array[i]);
   }
 
 //  printf("\n");
+  struct timespec start, end;
 
+  clock_gettime(CLOCK_MONOTONIC, &start);
   counting_sort(array, N, MAX_VAL);
+  clock_gettime(CLOCK_MONOTONIC, &end);
 /*
   printf("sorted array\n");
   for(int i=0; i<N; i++){
@@ -36,5 +43,7 @@ int main()
       }
   }
   printf("Sorted\n");
+  uint64_t elapsed = (end.tv_nsec - start.tv_nsec) + (end.tv_sec - start.tv_sec)*1000000000;
+  printf("time(us): %lu\n", elapsed/1000);
   
 }
